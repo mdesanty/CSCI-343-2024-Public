a76c92ec1063b33bdb6a4ba1f0d3aa7dd37b0b40
Michael DeSanty <michael.desanty@gmail.com>
2024-01-07 20:24:34 -0500

    Lecturcize 1 comments

--- Lecturcize1_YourFirstNodeServer/server.js
+++ Lecturcize1_YourFirstNodeServer/server.js
@@ -8,6 +8,16 @@ function requestHandler(req, res) {
 
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("<p>Hello.</p>");
+
+  /**
+   * res.end can also take a string to send the same way that .write does.
+   *
+   * This means instead of writing:
+   * res.write("<p?Have a nice day!</p>");
+   * res.end();
+   *
+   * We can simpley write:
+   */
   res.end("<p>Have a nice day!</p>");
 }
 
