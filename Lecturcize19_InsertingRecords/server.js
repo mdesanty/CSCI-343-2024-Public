96e87cf9a67e0ec1c43d8e53d799a0ce6f575177
Michael DeSanty <michael.desanty@gmail.com>
2024-01-08 21:40:25 -0500

    Lecturcize 19

---
+++ Lecturcize19_InsertingRecords/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+const pgClient = require("./pgClient");
+
+const app = express();
+
+/**
+ * This is a library to parse the body of requests.
+ *
+ * If you recall from Advanced Web Dev, post and put requests use the body for
+ * the data instead of the query.
+ */
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/books", createBook);
+
+const listener = app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(`Server listening at ${listener.address().address}:${listener.address().port}`);
+});
+
+function createBook(req, res) {
+  const book = req.body;
+
+  pgClient.query("INSERT INTO books (name, author) VALUES ($1, $2)", [book.name, book.author])
+    .then((results) => {
+      res.status(201).json({ message: "Book created successfully." });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: `We encountered an error with your request: ${ error }.` });
+    });
+}
