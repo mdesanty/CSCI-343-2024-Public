d255458edd18623012da0c5c3aa8477cfe0900b3
Michael DeSanty <michael.desanty@gmail.com>
2024-04-05 10:39:36 -0400

    Add cookie parser to 9.1

--- Lecturcizes/9 User Authentication/1 API User Authentication with JWT/api/src/server.js
+++ Lecturcizes/9 User Authentication/1 API User Authentication with JWT/api/src/server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
 const express = require("express");
+const cookies = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
+app.use(cookies());
 
 const authRoutes = require('./routes/authRoutes');
 app.use('/auth', authRoutes);
